a23d38ba9ecba65e4628ab596f6784820065ac5b
JuanPGonzalez <99921443+JuanPGonzalez@users.noreply.github.com>
2022-09-13 17:06:38 -0300

    Correcion de errores

--- ProyectoIntegrador_GonzalezJuanPablo/Backend/src/main/java/com/jpgportfolio/jpg/Controller/PersonaController.java
+++ ProyectoIntegrador_GonzalezJuanPablo/Backend/src/main/java/com/jpgportfolio/jpg/Controller/PersonaController.java
@@ -62,5 +62,8 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }
-    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)3);
+    }
 }
--- ProyectoIntegrador_GonzalezJuanPablo/Frontend/src/app/service/token.service.ts
+++ ProyectoIntegrador_GonzalezJuanPablo/Frontend/src/app/service/token.service.ts
@@ -34,17 +34,17 @@ export class TokenService {
   public setAuthorities(authorities: string[]):void{
     window.sessionStorage.removeItem(AUTHORITIES_KEY);
     window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[]{
+    this.roles = [];
+    if(sessionStorage.getItem(AUTHORITIES_KEY)){
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) => {
+        this.roles.push(authority.authority);
+      });
     }
-    
-    public getAuthorities(): string[]{
-      this.roles = [];
-      if(sessionStorage.getItem(AUTHORITIES_KEY)){
-        JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) => {
-          this.roles.push(authority.authority);
-        });
-      }
-      return this.roles;
-    }
+    return this.roles;
+  }
 
   public logOut(): void{
     window.sessionStorage.clear();
